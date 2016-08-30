dad3903f02191ae4f66185e5dadede732f697bcb
Martin080793 <martinschlidt@web.de>
2016-08-30 16:16:38 +0200

    Add files via upload

---
+++ route/Ort.java
@@ -0,0 +1,72 @@
+import java.util.Set;
+
+import java.util.TreeSet;
+
+public class Ort {
+
+	int name;
+
+	int vorgaenger;
+
+	int weglaenge;
+
+	Set<Weg> wege;
+
+	boolean besucht;
+
+	@Override
+
+	public String toString() {
+
+		return "Ort " + name + " (Vorgänger: " + (0 == vorgaenger ? "[]" : vorgaenger)
+				+ ") [weglänge total von start aus: " + weglaenge + "] <besucht: " + besucht + ">";
+
+	}
+
+	public Ort(int ortA) {
+
+		this.name = ortA;
+
+		this.vorgaenger = 0;
+
+		this.weglaenge = Integer.MAX_VALUE;
+
+		this.wege = new TreeSet<Weg>();
+
+		besucht = false;
+
+	}
+
+	public void addWeg(int ortB, int abst) {
+
+		if (null == wege) {
+
+			wege = new TreeSet<Weg>();
+
+		}
+
+		Weg w = new Weg();
+
+		w.ortA = this.name;
+
+		w.ortB = ortB;
+
+		w.weglaenge = abst;
+
+		wege.add(w);
+
+	}
+
+	public void setzeVorgaenger(int vorgaenger) {
+
+		this.vorgaenger = vorgaenger;
+
+	}
+
+	public void setzeWeglaenge(int weglaenge) {
+
+		this.weglaenge = weglaenge;
+
+	}
+
+}
---
+++ route/Starter.java
@@ -0,0 +1,9 @@
+
+public class Starter {
+	
+
+	public static void main(String[] args) {
+new Dijkstra(5,13);
+	}
+
+}
